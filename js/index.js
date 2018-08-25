9fd6f0e5246da4348e8a0ae6818fd3807a4dd5ff
Sanny Trinh <strinh@sfu.ca>
2018-08-24 18:30:05 -0700

    resopnisve mobile corpoarte teams section & social section

--- js/index.js
+++ js/index.js
@@ -1,22 +1,20 @@
-<script>
-  // init controller
-  var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 100}});
+
+  var controller = new ScrollMagic.Controller({globalSceneOptions: {duration: 500}});
 
   // build scenes
   new ScrollMagic.Scene({triggerElement: "#howitworks"})
-          .setClassToggle("#high1", "active") // add class toggle
-          .addIndicators() // add indicators (requires plugin)
+          .setClassToggle("#high1", "active")
+          // .addIndicators()
           .addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#about"})
-          .setClassToggle("#high2", "active") // add class toggle
-          .addIndicators() // add indicators (requires plugin)
+          .setClassToggle("#high2", "active")
+          // .addIndicators()
           .addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#corporate"})
-          .setClassToggle("#high3", "active") // add class toggle
-          .addIndicators() // add indicators (requires plugin)
+          .setClassToggle("#high3", "active")
+          // .addIndicators()
           .addTo(controller);
   new ScrollMagic.Scene({triggerElement: "#participant"})
-          .setClassToggle("#high4", "active") // add class toggle
-          .addIndicators() // add indicators (requires plugin)
+          .setClassToggle("#high4", "active")
+          // .addIndicators()
           .addTo(controller);
-</script>
